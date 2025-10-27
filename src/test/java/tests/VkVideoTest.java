61853025a55637145df9bf92022f924dd0edd246
Dina <sd@myexe.net>
2025-10-27 18:20:45 +0300

    Change test method

--- src/test/java/tests/VkVideoTest.java
+++ src/test/java/tests/VkVideoTest.java
@@ -14,6 +14,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.qameta.allure.Owner;
 import io.qameta.allure.selenide.AllureSelenide;
 import pages.vkvideo.VkVideoMainPage;
+import utils.NetworkConnectionUtils;
 
 @Owner("Dina Kholomkina")
 public class VkVideoTest extends BaseTest {
@@ -40,6 +41,7 @@ public class VkVideoTest extends BaseTest {
 
     @AfterEach
     public void tearDown() {
+        NetworkConnectionUtils.enableNetworkConnection();
         closeWebDriver();
     }
 }
